2ba328621b3bab41f4ac74e69ce55e844126c734
Daniel Machado <danielmcis@gmail.com>
2023-10-23 02:29:50 +0000

    wip

---
+++ src/app/Navbar/ShoppingCartButton.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ShoppingCart } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/format";
+import Link from "next/link";
+
+interface ShoppingCartButtonProps {
+  cart: ShoppingCart | null;
+}
+
+export default function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
+  function closeDropdown() {
+    const elem = document.activeElement as HTMLElement;
+    if (elem) {
+      elem.blur();
+    }
+  }
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-circle btn-ghost">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M22.713,4.077A2.993,2.993,0,0,0,20.41,3H4.242L4.2,2.649A3,3,0,0,0,1.222,0H1A1,1,0,0,0,1,2h.222a1,1,0,0,1,.993.883l1.376,11.7A5,5,0,0,0,8.557,19H19a1,1,0,0,0,0-2H8.557a3,3,0,0,1-2.82-2h11.92a5,5,0,0,0,4.921-4.113l.785-4.354A2.994,2.994,0,0,0,22.713,4.077ZM21.4,6.178l-.786,4.354A3,3,0,0,1,17.657,13H5.419L4.478,5H20.41A1,1,0,0,1,21.4,6.178Z" />
+            <circle cx="7" cy="22" r="2" />
+            <circle cx="17" cy="22" r="2" />
+          </svg>
+          <span className="badge indicator-item badge-sm">
+            {cart?.size || 0}
+          </span>
+        </div>
+      </label>
+      <div
+        tabIndex={0}
+        className="card dropdown-content card-compact z-30 mt-3 w-52 bg-base-100 shadow"
+      >
+        <div className="card-body">
+          <span className="text-lg font-bold">{cart?.size || 0} Items</span>
+          <span className="text-info">
+            Subtotal: {formatPrice(cart?.subtotal || 0)}
+          </span>
+          <div className="card-actions">
+            <Link
+              href="/cart"
+              className="btn btn-primary btn-block"
+              onClick={closeDropdown}
+            >
+              View Cart
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "./Footer";
+import Navbar from "./Navbar/Navbar";
+import SessionProvider from "./SessionProvider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+        <SessionProvider>
+          <Navbar />
+          <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
